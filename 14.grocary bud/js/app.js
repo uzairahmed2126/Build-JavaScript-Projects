20d22913dc86529dcfd98a3bc18850b87e067617
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-10-07 18:08:47 +0530

    .

---
+++ 14.grocary bud/js/app.js
@@ -0,0 +1,14 @@
+const alert = document.querySelector('.alert')
+const form = document.querySelector('.grocery-form')
+const grocery = document.getElementById('.grocery')
+const submitBtn = document.querySelector('.submit-btn')
+const container = document.querySelector('.grocery-container')
+const list = document.querySelector('.grocery-list')
+const clearBtn = document.querySelector('.clear-btn')
+submitBtn.addEventListener('click',()=>{
+    if(submitBtn==''){
+        
+    }else{
+
+    }
+})
