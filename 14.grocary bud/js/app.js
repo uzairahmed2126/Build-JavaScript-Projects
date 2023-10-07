87dc94bb929c350249f96c58743ca79dcfe1b597
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-10-08 00:55:59 +0530

    .

--- 14.grocary bud/js/app.js
+++ 14.grocary bud/js/app.js
@@ -1,14 +1,27 @@
-const alert = document.querySelector('.alert')
-const form = document.querySelector('.grocery-form')
-const grocery = document.getElementById('.grocery')
-const submitBtn = document.querySelector('.submit-btn')
-const container = document.querySelector('.grocery-container')
-const list = document.querySelector('.grocery-list')
-const clearBtn = document.querySelector('.clear-btn')
-submitBtn.addEventListener('click',()=>{
-    if(submitBtn==''){
-        
-    }else{
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
-    }
-})
+let editElement;
+let editFlag = false;
+let editID = "";
+
+
+
+form.addEventListener("submit", addItem);
+
+// clearBtn.addEventListener("click", clearItems);
+
+// window.addEventListener("DOMContentLoaded", setupItems);
+
+
+
+
+function addItem(e) {
+    e.preventDefault();
+    console.log(grocery.value)
+}
